bfaad41a798fdbc928cdf894df4cf270b022e40c
Ceranor <streiff.ch-6@bluewin.ch>
2020-03-29 15:24:16 +0200

    Create documentation for CsvParser

--- src/parser/CsvParser.java
+++ src/parser/CsvParser.java
@@ -14,11 +14,27 @@ public class CsvParser {
     private static final char[] ILLEGAL_CHARACTERS = {' ', '_'};
     private static final char[] REPLACEMENT_CHARACTERS = {'-', '-'};
 
+    /**
+     * Reads a CSV file and parses it into a CsvTable object.
+     *
+     * @param path The path of the file to be read
+     * @return The read CSV file as a CsvTable
+     * @throws IOException            Thrown if there is an error while reading the file
+     * @throws CsvValidationException Thrown if there is an error while validating the CSV
+     */
     public static CsvTable readCsvFromFile(String path) throws IOException, CsvValidationException {
         BufferedReader reader = new BufferedReader(new FileReader(path));
         return readCsv(reader);
     }
 
+    /**
+     * Reads CSV-formatted input from any reader and parses it onto a CsvTable object.
+     *
+     * @param reader A reader which reads from CSV-formatted data
+     * @return The read CSV input as a CsvTable
+     * @throws IOException            Thrown if there is an error while reading the file
+     * @throws CsvValidationException Thrown if there is an error while validating the CSV
+     */
     public static CsvTable readCsv(Reader reader) throws IOException, CsvValidationException {
         CSVReader csvReader = new CSVReader(reader);
         String[] currentLine = csvReader.readNext();
@@ -30,6 +46,12 @@ public class CsvParser {
         return table;
     }
 
+    /**
+     * Removes all illegal characters (defined in constants) from a string array and replaces them.
+     * Serves to make the headers FHIR-compliant.
+     *
+     * @param headers The array of headers to remove illegal characters from
+     */
     private static void removeIllegalCharactersFromHeaders(String[] headers) {
         for (String currentHeader : headers) {
             for (int i = 0; i < ILLEGAL_CHARACTERS.length; i++) {
