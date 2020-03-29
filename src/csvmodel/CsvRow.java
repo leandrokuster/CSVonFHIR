d5b95ff4b0a5d931a51281cc77f0b301b944135e
Ceranor <streiff.ch-6@bluewin.ch>
2020-03-29 14:51:18 +0200

    Create documentation for CsvRow

--- src/csvmodel/CsvRow.java
+++ src/csvmodel/CsvRow.java
@@ -6,11 +6,23 @@ public class CsvRow {
     private CsvTable table;
     private Hashtable<String, String> rowMap;
 
+    /**
+     * Generates a new row to include in a CsvTable.
+     *
+     * @param table  The table to insert the row in
+     * @param rowMap A map of the column headers to the row data contents
+     */
     public CsvRow(CsvTable table, Hashtable<String, String> rowMap) {
         this.table = table;
         this.rowMap = rowMap;
     }
 
+    /**
+     * Retrieves data from a certain column in the row.
+     *
+     * @param header The column header for the attribute
+     * @return The data in the given column
+     */
     public String getAttribute(String header) {
         if (table.getHeaders().contains(header)) {
             return rowMap.get(header);
@@ -19,6 +31,12 @@ public class CsvRow {
         }
     }
 
+    /**
+     * Stores a piece of data in a certain column in the row, replacing any current entry.
+     *
+     * @param header    The column header to store the data in
+     * @param attribute The data to store in the given column
+     */
     public void putAttribute(String header, String attribute) {
         if (table.getHeaders().contains(header)) {
             rowMap.put(header, attribute);
