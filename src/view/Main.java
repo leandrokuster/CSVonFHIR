4945f38256130a68ef5bea05c4253ca35387a742
Ceranor <streiff.ch-6@bluewin.ch>
2020-03-30 11:24:18 +0200

    Imlement storage of data JSON

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,43 +1,44 @@
 package view;
 
+import com.google.gson.JsonArray;
 import com.opencsv.exceptions.CsvValidationException;
 import csvmodel.CsvTable;
 import org.hl7.fhir.r4.model.StructureDefinition;
-import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
 import parser.CsvParser;
 import parser.CsvToJsonParser;
 import parser.CsvToStructureDefinitionParser;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
 
 public class Main {
     private static final String INPUT_PATH_FLAG = "-i";
     private static final String TYPE_FLAG = "-t";
     private static final String STRUCTURE_DEFINITION_PATH_FLAG = "-s";
+    private static final String DATA_PATH_FLAG = "-d";
+
+    private static final String DEFAULT_STRUCTURE_DEFINITION_PATH = "./structure-definition.json";
+    private static final String DEFAULT_DATA_PATH = "./data.json";
 
     public static void main(String[] args) {
         String csvInputPath = getCsvInputPath(args);
         String type = getType(args);
         String structureDefinitionOutputPath = getStructureDefinitionOutputPath(args);
+        String dataJsonPath = getDataJsonPath(args);
 
         System.out.println("Reading CSV...");
         CsvTable inputTable = parseCsvFromFile(csvInputPath);
 
         System.out.println("Generating structure definition...");
         StructureDefinition structureDefinition = CsvToStructureDefinitionParser.generateStructureDefinitionFromCsv(inputTable, type);
-        if (structureDefinitionOutputPath != null) {
-            try {
-                String structureDefinitionJson = CsvToStructureDefinitionParser.generateStructureDefinitionJson(structureDefinition);
-                writeToFile(structureDefinitionOutputPath, structureDefinitionJson);
-            } catch (IOException e) {
-                System.out.println("Warning: Generation of structure definition JSON file failed.");
-            }
-        }
+        writeStructureDefinition(structureDefinitionOutputPath, structureDefinition);
 
-        List<JSONObject> inputTableJson = CsvToJsonParser.generateJSONFromCSV(inputTable, type);
-        // TODO: Retrieve map in correct format, plug inputTableJSON and map into transformator/validator, save output.
+        System.out.println("Generating data file...");
+        JSONArray inputTableJson = CsvToJsonParser.generateJSONFromCSV(inputTable, type);
+        writeDataJson(dataJsonPath, inputTableJson);
+        
+        // TODO: Save everything to a file, then pass it as a map file path and a single JSON array to transformator
     }
 
     private static String getCsvInputPath(String[] args) {
@@ -64,8 +65,17 @@ public class Main {
         try {
             return getParameter(STRUCTURE_DEFINITION_PATH_FLAG, args);
         } catch (IllegalArgumentException e) {
-            System.out.println("Warning: Structure definition output argument (-s) not found, no file will be generated.");
-            return null;
+            System.out.println("Warning: Structure definition output argument (-s) not found, using default path.");
+            return DEFAULT_STRUCTURE_DEFINITION_PATH;
+        }
+    }
+
+    private static String getDataJsonPath(String[] args) {
+        try {
+            return getParameter(DATA_PATH_FLAG, args);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Warning: Data output argument (-d) not found, using default path.");
+            return DEFAULT_DATA_PATH;
         }
     }
 
@@ -78,14 +88,29 @@ public class Main {
         throw new IllegalArgumentException("Given parameter was not found.");
     }
 
-    private static void writeToFile(String path, String input) {
+    private static void writeStructureDefinition(String path, StructureDefinition definition) {
         try {
-            FileWriter fileWriter = new FileWriter(path);
-            fileWriter.write(input);
-            fileWriter.close();
+            String structureDefinitionJson = CsvToStructureDefinitionParser.generateStructureDefinitionJson(definition);
+            writeToFile(path, structureDefinitionJson);
         } catch (IOException e) {
-            System.out.println("Warning: Write to " + path + "failed.");
+            System.err.println("Error: Generation of structure definition JSON file failed.");
+            System.exit(-1);
         }
+    }
+
+    private static void writeDataJson(String path, JSONArray data) {
+        try {
+            writeToFile(path, data.toJSONString());
+        } catch (IOException e) {
+            System.err.println("Error: Generation of data JSON file failed.");
+            System.exit(-1);
+        }
+    }
+
+    private static void writeToFile(String path, String input) throws IOException {
+        FileWriter fileWriter = new FileWriter(path);
+        fileWriter.write(input);
+        fileWriter.close();
     }
 
     private static CsvTable parseCsvFromFile(String path) {
@@ -100,5 +125,4 @@ public class Main {
         }
         throw new IllegalStateException();
     }
-
 }
